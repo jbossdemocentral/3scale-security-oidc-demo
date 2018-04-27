9e883a0e9e56339cbac5efc1298ebe3a484dfadb
Hugo Guerrero <hguerrer@redhat.com>
2018-04-27 13:17:08 -0500

    update to handle build and run phases

--- projects/accidentalert-ui/gulpfile.js
+++ projects/accidentalert-ui/gulpfile.js
@@ -1,49 +1,49 @@
-var gulp               = require('gulp');             
-var fs                 = require('fs');
-var es                 = require('event-stream');
-var path               = require('path');
-var _                  = require('lodash');
-var uglify             = require('gulp-uglify'); 
-var sass               = require('gulp-sass');
-var cleanCSS           = require('gulp-clean-css');
-var rename             = require('gulp-rename'); 
-var autoprefixer       = require('gulp-autoprefixer');
-var include            = require('gulp-include');
-var notify             = require("gulp-notify");
-var imagemin           = require("gulp-imagemin"); 
-var livereload         = require('gulp-livereload');
-var sourcemaps         = require('gulp-sourcemaps');
-var webserver          = require('gulp-webserver');
-var clean              = require('gulp-clean');
-var replace            = require('gulp-string-replace');
-var yn                 = require('yn');
+var gulp = require('gulp');
+var fs = require('fs');
+var es = require('event-stream');
+var path = require('path');
+var _ = require('lodash');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var include = require('gulp-include');
+var notify = require("gulp-notify");
+var imagemin = require("gulp-imagemin");
+var livereload = require('gulp-livereload');
+var sourcemaps = require('gulp-sourcemaps');
+var webserver = require('gulp-webserver');
+var clean = require('gulp-clean');
+var replace = require('gulp-string-replace');
+var yn = require('yn');
 
-var srcPath            = 'templates/src/';            // Path to the source files
-var distPath           = 'templates/dist/';            // Path to the distribution files
+var srcPath = 'templates/src/';            // Path to the source files
+var distPath = 'templates/dist/';            // Path to the distribution files
 var paths = {
     webroot: "./wwwroot/",
     node_modules: "./node_modules/"
 };
 
 // Paths that gulp should watch
-var watchPaths        = {
-    scripts:     [
-        srcPath+'assets/js/*.js',
-        srcPath+'assets/js/**/*.js'
+var watchPaths = {
+    scripts: [
+        srcPath + 'assets/js/*.js',
+        srcPath + 'assets/js/**/*.js'
     ],
-    images:     [
-        srcPath+'assets/img/**'
+    images: [
+        srcPath + 'assets/img/**'
     ],
-    sass:         [
-        srcPath+'assets/sass/*.scss',
-        srcPath+'assets/sass/**/*.scss'
+    sass: [
+        srcPath + 'assets/sass/*.scss',
+        srcPath + 'assets/sass/**/*.scss'
     ],
-    fonts:      [
-        srcPath+'assets/fonts/**'
+    fonts: [
+        srcPath + 'assets/fonts/**'
     ],
-    html:          [
-        srcPath+'**/*.html',
-        srcPath+'**/*.php'
+    html: [
+        srcPath + '**/*.html',
+        srcPath + '**/*.php'
     ]
 };
 
@@ -69,7 +69,7 @@ gulp.task('sass', ['clean'], function () {
 });
 
 // Javscript task
-gulp.task('scripts', ['clean'], function(){
+gulp.task('scripts', ['clean'], function () {
     return gulp
         .src(srcPath + 'assets/js/*.js')
         .pipe(include())
@@ -126,7 +126,7 @@ gulp.task("copy:libs", ['clean'], function () {
         ]
     };
 
-    _(assets).forEach(function(assets, type) {
+    _(assets).forEach(function (assets, type) {
         gulp
             .src(assets)
             .pipe(gulp.dest(distPath + 'assets/' + type));
@@ -136,7 +136,7 @@ gulp.task("copy:libs", ['clean'], function () {
 });
 
 // Watch task
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch(watchPaths.scripts, ['scripts']);
     gulp.watch(watchPaths.images, ['images']);
     gulp.watch(watchPaths.sass, ['sass']);
@@ -148,20 +148,29 @@ gulp.task('watch', function() {
     return;
 });
 
-gulp.task('build', ['copy:libs', 'scripts', 'images', 'sass', 'fonts', 'html'], function() {
+gulp.task('build', ['copy:libs', 'scripts', 'images', 'sass', 'fonts', 'html'], function () {
     return;
 });
 
 gulp.task('serve', ['build'], function () {
     return gulp.src(distPath)
-      .pipe(webserver({
-        host: process.env.HOSTNAME || 'localhost',
-        port: 8080,
-        livereload: yn(process.env.LIVERELOAD, { default: false })
-      }));
-  });
+        .pipe(webserver({
+            host: process.env.HOSTNAME || 'localhost',
+            port: 8080,
+            livereload: yn(process.env.LIVERELOAD, { default: false })
+        }));
+});
 
-gulp.task('serve:watch', ['watch', 'serve'], function(){
+gulp.task('serve:dist', function () {
+    return gulp.src(distPath)
+        .pipe(webserver({
+            host: process.env.HOSTNAME || 'localhost',
+            port: 8080,
+            livereload: yn(process.env.LIVERELOAD, { default: false })
+        }));
+});
+
+gulp.task('serve:watch', ['watch', 'serve'], function () {
     return;
 });
 
